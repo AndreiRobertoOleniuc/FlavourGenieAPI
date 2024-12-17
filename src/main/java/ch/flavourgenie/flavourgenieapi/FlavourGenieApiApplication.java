4c4c1358ac277b88d829d1414aa28999b4e16def
Andrei Roberto Oleniuc <andreioleniucroberto@gmail.com>
2024-12-17 19:38:35 +0100

    init commit

---
+++ src/main/java/ch/flavourgenie/flavourgenieapi/FlavourGenieApiApplication.java
@@ -0,0 +1,13 @@
+package ch.flavourgenie.flavourgenieapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlavourGenieApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlavourGenieApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/flavourgenie/flavourgenieapi/FlavourGenieApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.flavourgenie.flavourgenieapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlavourGenieApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
