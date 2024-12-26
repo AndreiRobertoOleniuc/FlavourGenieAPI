a06f8994811d71605b13e7b83ec7e12ef1e0eb7f
Andrei Roberto Oleniuc <andreioleniucroberto@gmail.com>
2024-12-26 23:06:00 +0200

    chore: enable caching

--- src/main/java/ch/flavourgenie/flavourgenieapi/FlavourGenieApiApplication.java
+++ src/main/java/ch/flavourgenie/flavourgenieapi/FlavourGenieApiApplication.java
@@ -2,8 +2,10 @@ package ch.flavourgenie.flavourgenieapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class FlavourGenieApiApplication {
 
     public static void main(String[] args) {
